7798b4a19404a26ccf0547ef469ebb202bf8fb82
Gabriel Inthurn <gabriel_inthurn@estudante.sesisenai.org.br>
2022-02-22 21:06:14 -0300

    JPA Support
    This commit will add the JPA support

---
+++ src/main/java/br/com/inthurn/VilaDevInHouse/model/entity/User.java
@@ -0,0 +1,60 @@
+package br.com.inthurn.VilaDevInHouse.model.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String username;
+    private String password;
+
+    @ManyToMany
+    private List<Role> roles;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, List<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
